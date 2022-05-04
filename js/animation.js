6de4571c97974e2dd0cb4b686feac72a6481ab13
Sayak Naskar <sayaknaskar@gmail.com>
2022-05-04 22:32:26 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -3,9 +3,9 @@
 /******************************************************* SETUP ************************************************************/
 
 const config = {
-    name: "Friend", // actual name of the recipient (Mandatory)
+    name: "Ranita", // actual name of the recipient (Mandatory)
 
-    nickname: "Buddy", // nickname(optional)
+    nickname: "Puti", // nickname(optional)
 
     pic: "", //image url of recipients (Mandatory)
 
